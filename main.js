9c35e80385f0a7ad040941bab608e20c31012b3f
AzareelJames <azareeljameslbasalo@gmail.com>
2025-07-27 18:49:27 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var time;
+var datetime;
+var hour;
+var day;
+document.body.style.fontSize = `100px`;
+document.body.style.textAlign = `center`;
+document.body.innerText = `Loading...`;
+document.title = `Clock`;
+function mainloop(){
+    setTimeout(() =>{
+        
+        time = new Date().toLocaleTimeString();
+        datetime = new Date().toLocaleDateString();
+        hour = new Date().getHours();
+        hour > 0 && hour < 6? day = `🌑`
+        : hour > 5 && hour < 12? day = `☀️`:
+        hour > 11 && hour < 18? day = `🌇`
+        : hour > 17? day = `🌙` : day = `?`;
+
+        if (day === `🌙` || day === `🌑`){
+            document.body.style.color = `#ffffff`;
+            document.body.style.backgroundColor = `#000000`;
+        } else if (day === `☀️`){
+            document.body.style.color = `#00000`;
+            document.body.style.backgroundColor = `#fffff`;
+        } else if (day === `🌇`){
+            document.body.style.color = `#ffffff`;
+            document.body.style.backgroundColor = `#383838`;
+        } else if (day === `?`){
+            throw Error(`Unexpected Error`)
+        }
+
+        document.body.innerText = `${time}\n${datetime}\n${day}`;
+        mainloop();
+    }, 265);
+}
+
+mainloop();
